2cae562e02b9bb1bc1c9cacda9350748d027bbc9
gabaaa <not-set>
2020-10-19 20:13:32 -0500

    adding base files

---
+++ __init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# authors: Gabriel Auger
+# name: ssh-looper
+# licenses: MIT 
+__version__ = "0.0.0"
+
+# from .dev.ssh-looper import ssh-looper
+# from .gpkgs import message as msg
+# from .gpkgs.options import Options
