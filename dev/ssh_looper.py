7a1524d986ed505d540814365b497c1f02797ca4
gabaaa <gabaaa.dev@gmail.com>
2023-06-23 21:48:53 -0500

    added --unknown host argument

--- dev/ssh_looper.py
+++ dev/ssh_looper.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 from pprint import pprint
+import json
 import os
 import re
+import shlex
+import socket
+import signal
+import subprocess
 import sys
+import tempfile
 import time
 import platform
 
@@ -45,45 +51,105 @@ def resolve(hostname, dns):
 
     return computer_ip
 
-def ssh_looper(cmd, dns=None):
+def ssh_looper_clear():
+    lines=shell.cmd_get_value("ps auxfww").splitlines()
+    python_cmds=[]
+    ssh_cmds=[]
+    for line in lines:
+        if "ssh -N -L" in line or "ssh -N -R" in line:
+            elems=line.split()
+            pid=int(elems[1])
+            cmd=" ".join(elems[10:])
+            if "python" in cmd:
+                python_cmds.append([pid, cmd])
+            else:
+                ssh_cmds.append([pid, cmd])
+
+    for cmds in [python_cmds, ssh_cmds]:
+        for pid, cmd in cmds:
+            os.kill(pid, signal.SIGTERM)
+            msg.success("ssh-looper: cleared: '{}'".format(cmd))
+
+def ssh_looper(cmd, dns=None, unknown_host=False):
     # ssh -N -L {port}:localhost:{port} {user}@{ip_name}
-    # ssh {user}@ip_name -N -R {port}:localhost:{port}
+    # ssh -N -R {port}:localhost:{port} {user}@ip_name
     cmd=cmd.strip()
-    reg=re.match(r"^(?P<cmd_start>ssh\s+-N\s+-L\s+[0-9]+:localhost:[0-9]+\s+.+?@)(?P<network>[^ ]+)$", cmd)
+    reg=re.match(r"^(?P<cmd_start>ssh\s+-N\s+-(?P<sshtype>L|R)\s+(?P<leftport>[0-9]+):localhost:(?P<rightport>[0-9]+)\s+.+?@)(?P<network>[^ ]+)(?P<key>\s+-i\s+[^ ]+)?$", cmd)
     if not reg:
-        reg=re.match(r"^(?P<cmd_start>ssh\s+.+?@)(?P<network>[^ ]+)(?P<cmd_end>\s+-N\s+-R\s+[0-9]+:localhost:[0-9]+)$", cmd)
-        if not reg:
-            msg.error("ssh cmd is not supported see readme '{}'".format(cmd), exit=1)
+        msg.error("ssh-looper: ssh cmd is not supported see readme '{}'".format(cmd), exit=1)
+
+    sshtype=reg.group("sshtype")
+    localport=None
+    remoteport=None
+    pid_script=os.getpid()
+    if sshtype == "L":
+        localport=int(reg.group("leftport"))
+        remoteport=int(reg.group("rightport"))
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = s.connect_ex(('127.0.0.1', localport))
+        is_open=(result == 0)
+        s.close()
+        if is_open:
+            msg.error("ssh-looper: port '{}' is already in use for command '{}'.".format(localport, cmd))
+            sys.exit(1)
+    else:
+        localport=int(reg.group("rightport"))
+        remoteport=int(reg.group("leftport"))
+        output=shell.cmd_get_value("ps -ef")
+        count=1
+        for line in output.splitlines():
+            if count > 1:
+                elems=line.split()
+                pid=int(elems[1])
+                if ("{}:localhost:{}".format(remoteport, localport) in line and pid != pid_script):
+                    msg.error("ssh-looper: port '{}' is already is already forwarded for command '{}'.".format(localport, cmd))
+                    sys.exit(1)
+            count+=1
 
     network=reg.group("network")
     cmd_start=reg.group("cmd_start")
     cmd_end=""
     if "cmd_end" in reg.groupdict():
         cmd_end=reg.group("cmd_end")
+        if cmd_end is None:
+            cmd_end=""
 
+    key=""
+    if "key" in reg.groupdict():
+        key=reg.group("key")
+        if key is None:
+            key=""
+
+    pause=10
     try:
         while True:
             network_to_ping=network
             if dns is not None:
                 resolved_network=resolve(network_to_ping, dns)
                 if resolved_network is None:
-                    time.sleep(5)
+                    time.sleep(pause)
                     continue
                 else:
                     network_to_ping=resolved_network
             if short_ping(network_to_ping) is True:
                 try:
-                    tmp_cmd=cmd_start+network_to_ping+cmd_end
+                    # ExitOnForwardFailure allows ssh to fail on forward failure
+                    tmp_cmd=cmd_start+network_to_ping+cmd_end+key+" -o ExitOnForwardFailure=yes"
+
+                    if unknown_host is True:
+                        tmp_cmd+=" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
                     print(tmp_cmd)
-                    os.system(tmp_cmd)
-                    time.sleep(5)
+                    proc = subprocess.Popen(shlex.split(tmp_cmd))
+                    proc.communicate()
+                    time.sleep(pause)
                 except KeyboardInterrupt:
                     break
                 except BaseException as e:
                     print(e)
+                    time.sleep(pause)
                     continue
             else:
-                time.sleep(10)
+                time.sleep(pause)
     except KeyboardInterrupt:
         pass
 
